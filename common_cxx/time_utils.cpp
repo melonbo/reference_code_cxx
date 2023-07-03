49fc911be9984bde22c3f7d4f8710ec296909af0
gulq <gulq@PC0097>
2023-07-03 18:36:54 +0800

    时间相关操作

---
+++ common_cxx/time_utils.cpp
@@ -0,0 +1,60 @@
+#include "time_utils.h"
+
+
+void wrapper_time_count(void(*fun)())
+{
+    auto start = high_resolution_clock::now();
+
+    fun();
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
+}
+
+void wrapper_time_count(void(*fun)(char*, char*), char* str1, char* str2)
+{
+    auto start = high_resolution_clock::now();
+
+    fun(str1, str2);
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
+}
+
+void getSysTime(char* year, char* mon, char* day, char* hour, char* min, char* sec)
+{
+    time_t timer;
+    struct tm* t_tm;
+    time(&timer);
+    t_tm = localtime(&timer);
+    *year = t_tm->tm_year+1900-2000;
+    *mon  = t_tm->tm_mon+1;
+    *day  = t_tm->tm_mday;
+    *hour = t_tm->tm_hour;
+    *min  = t_tm->tm_min;
+    *sec  = t_tm->tm_sec;
+}
+
+unsigned int getSystemSeconds()
+{
+    time_t timer;
+    time(&timer);
+    return timer;
+}
+
+unsigned int getFormatSeconds(char year, char mon, char day, char hour, char min, char sec)
+{
+    struct tm formatTime;
+    formatTime.tm_year = year + 2000 - 1900;
+    formatTime.tm_mon = mon - 1;
+    formatTime.tm_mday = day;
+    formatTime.tm_hour = hour;
+    formatTime.tm_min = min;
+    formatTime.tm_sec = sec;
+
+    return mktime(&formatTime);
+}
---
+++ common_cxx/time_utils.h
@@ -0,0 +1,19 @@
+#ifndef TIME_UTILS_H
+#define TIME_UTILS_H
+
+#include <iostream>
+#include <chrono>
+#include <QThread>
+#include <time.h>
+#include <ctime>
+
+using namespace std;
+using namespace std::chrono;
+
+void wrapper_time_count(void(*fun)());//计算函数运行时间
+void wrapper_time_count(void(*fun)(char*, char*), char* str1, char* str2);//计算函数运行时间
+void getSysTime(char* year, char* mon, char* day, char* hour, char* min, char* sec);//获取系统时间
+unsigned int getSystemSeconds();//获取系统时间,1970年到现在的秒数
+unsigned int getFormatSeconds(char year, char mon, char day, char hour, char min, char sec);//格式时间转换为秒数
+
+#endif // TIME_UTILS_H
